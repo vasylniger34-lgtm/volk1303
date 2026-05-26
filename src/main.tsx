76ce64794f5a7d0f13ec517e881c87392bb02693
Boryslav <bcgmm@proton.me>
2026-05-26 14:16:07 +0300

    Fix Telegram Web App endless loading by calling ready()

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Hide Telegram splash screen and expand window
+if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
+  window.Telegram.WebApp.ready();
+  window.Telegram.WebApp.expand();
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
